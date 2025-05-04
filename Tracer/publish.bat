30aa268f4ebe272813b308af0cf267a13e84c113
Infarh <shmachilin@gmail.com>
2025-05-04 21:48:28 +0300

    Обновлдение скрипта публикации

--- Tracer/publish.bat
+++ Tracer/publish.bat
@@ -1,3 +1,11 @@
-@echo off
+﻿@echo off
 
 dotnet publish -v d /p:PublishProfile=Properties/PublishProfiles/FolderProfile.pubxml
+
+ if %errorlevel% neq 0 (
+    echo Release build error
+    pause
+    exit /b 5
+)
+
+pause
